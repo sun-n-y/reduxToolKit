1f2761e84226493b59170581e74619577bf902cf
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-16 12:57:51 -0500

    add modal logic

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import { calculateTotals } from './features/cart/cartSlice';
 import Modal from './components/Modal';
 
 function App() {
-  const cartItems = useSelector((store) => store.cart);
+  const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,7 +17,7 @@ function App() {
 
   return (
     <main>
-      <Modal />
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
