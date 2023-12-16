5d1dc04b9ee7b364c0f716e2b248a23c471b4107
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-16 12:39:02 -0500

    add modal structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CartContainer from './components/CartContainer';
 import Navbar from './components/Navbar';
 import { useEffect } from 'react';
 import { calculateTotals } from './features/cart/cartSlice';
+import Modal from './components/Modal';
 
 function App() {
   const cartItems = useSelector((store) => store.cart);
@@ -14,6 +15,7 @@ function App() {
 
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
